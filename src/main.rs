e74be3b28fcdf1e22a33ffc01722181364b23a06
zul.wasaya@gmail.com <zul.wasaya@gmail.com>
2021-02-13 17:54:52 +0000

    changed repo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     println!("Welcome to the Game! by zul wasaya using RUST language");
-    println!("using github repository rustfirst");
+    println!("using github repo rustfirst");
 }
