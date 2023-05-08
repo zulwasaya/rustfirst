d14651fc7b028663d75ecd983db83e2585b5eb98
zul wasaya <zul.wasaya@gmail.com>
2023-05-08 14:55:34 +0100

    updated source to include public(crate)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+pub(crate) fn main() {
     println!("Welcome to the Game! by zul wasaya using RUST language");
     println!("using github repo rustfirst");
     println!("testing github repo")
